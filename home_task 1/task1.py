4b27c9a17a066256a706c623ae96dc00e035a321
mubinanusratullayeva <mubinanusratullayeva1.@gmail.com>
2023-12-06 07:24:58 -0800

    task-1 leetcode

---
+++ home_task 1/task1.py
@@ -0,0 +1,11 @@
+from typing import List
+
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        for i in range(1, len(nums)):
+            nums[i] += nums[i-1]
+        return nums
+
+cl = Solution()
+
+print(cl.runningSum([1,2,3,4]))
