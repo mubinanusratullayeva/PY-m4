58c5b3093ce0d13fc3781c28c370c64e23a4256a
mubinanusratullayeva <mubinanusratullayeva1.@gmail.com>
2023-12-10 08:40:50 -0800

    replaced

--- home_task 3/task-2/config.py
+++
@@ -1,5 +0,0 @@
-HOST = "localhost"
-DBNAME = "n28_shopping"
-PORT = '5432'
-USERNAME = "postgres"
-PASSWORD = "postgres"                                                                                                                                                                            "
