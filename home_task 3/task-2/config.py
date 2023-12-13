15bbcd5bf8f182d9dc1b1d163cce350b857411c4
mubinanusratullayeva <mubinanusratullayeva1.@gmail.com>
2023-12-13 08:44:01 -0800

    not finished

--- home_task 3/task-2/config.py
+++ home_task 3/task-2/config.py
@@ -2,4 +2,6 @@ HOST = 'localhost'
 DBNAME = 'n28_shopping'
 PORT = '5432'
 USERNAME = 'postgres'
-PASSWORD = 'postgres'                                                                                                                                        "
+PASSWORD = 'postgres'
+
+
